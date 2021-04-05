c2d706610c97b1040ee490a5cac99cf6a5ba7b90
Trias <triasbrata@gmail.com>
2021-04-06 07:12:42 +0800

    u: scraper html to ignore ssl isue

--- src/scraper-html/scraper-html.module.ts
+++ src/scraper-html/scraper-html.module.ts
@@ -1,8 +1,10 @@
 import { HttpModule, Module } from '@nestjs/common';
+import { Agent } from 'https';
+import { HttpsProxyAgent } from 'https-proxy-agent';
 import { ScraperHtmlService } from './scraper-html.service';
 
 @Module({
-  imports: [HttpModule],
+  imports: [HttpModule.register({httpsAgent: new Agent({rejectUnauthorized:false})})],
   providers: [ScraperHtmlService],
   exports: [ScraperHtmlService],
 })
