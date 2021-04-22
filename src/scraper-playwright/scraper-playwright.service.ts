23c9d2b5cf4e8bfe6612a04dc6e7fe58be0b7962
Trias <triasbrata@gmail.com>
2021-04-23 07:12:39 +0800

    u: scraper

--- src/scraper-playwright/scraper-playwright.service.ts
+++ src/scraper-playwright/scraper-playwright.service.ts
@@ -1,15 +1,14 @@
-import { CleanerType } from '@lib/entities/schemas/pipe-rule';
-import { Injectable, Logger, OnModuleInit } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import { plainToClass } from 'class-transformer';
 import { resolve } from 'path';
-import { ElementHandle, Page } from 'playwright';
+import { Page } from 'playwright';
 import { cleanOrigin } from '../helpers/clear-origin';
 import { PlaywrightService } from '../playwright/playwright.service';
 import { PatternContainerParser } from './pattern-container';
 import { PatternContainer } from './types/patterns/pattern-container.type';
 import { PatternField } from './types/patterns/pattern.type';
 
-export interface evaluatePageOptions {
+export interface EvaluatePageOptions {
 
   url?: string;
   page?: Page;
@@ -17,6 +16,15 @@ export interface evaluatePageOptions {
   autoClose?: boolean;
   waitAfterConnected?: number;
   scrollToView?: boolean;
+  useURL?: boolean;
+  keyword?:string;
+  searchPattern?: string;
+  pageNow:number;
+  searchPatternType?: "css"|"xpath";
+}
+export interface EvaluatePageLazyOptions extends EvaluatePageOptions{
+  toPage: number,
+  patternProduct: PatternContainer
 }
 
 @Injectable()
@@ -26,7 +34,7 @@ export class ScraperPlaywrightService {
   constructor(private readonly playwrightService: PlaywrightService) {
   }
 
-  async evaluateWebsite<Out extends Record<string, any> = any>({ pattern, ...options }: evaluatePageOptions): Promise<[Out[], Page]> {
+  async evaluateWebsite<Out extends Record<string, any> = any>({ pattern, ...options }: EvaluatePageOptions): Promise<[Out[], Page]> {
     let page: Page;
     try {
       if (options.url && !options.page) {
@@ -39,7 +47,15 @@ export class ScraperPlaywrightService {
         this.logger.debug(`wait for ${options.waitAfterConnected}s after success load ${options.url}`)
         await new Promise(res => setTimeout(res, options.waitAfterConnected * 1000));
       }
+      if(options.useURL === false && options.pageNow === 1){
+        const pattern = this.makePattern(options.searchPattern, options.searchPatternType);
+        await page.waitForSelector(pattern,{state:"visible"});
+        await page.type(pattern, options.keyword,{delay:100});
+        await page.$(pattern).then(el => el.press("Enter"))
+        await page.waitForNavigation({waitUntil:"networkidle"});
+      }
       const urlNow = page.url();
+      const originUrl = cleanOrigin(new URL(urlNow).origin);
       const pContainer = this.makePattern(pattern.containerPattern, pattern.patternType)
       await page.waitForSelector(pContainer);
       if ((pattern.fields?.length || 0) < 1) {
@@ -47,7 +63,7 @@ export class ScraperPlaywrightService {
       }
       const nContainers = await page.$$(pContainer);
       if (nContainers.length < 1) {
-        await page.screenshot({ path: resolve('outs/fails/test.png') })
+        await page.screenshot({ path: resolve(`outs/fails/${originUrl}.png`) })
         if (options.autoClose) {
           await page.close();
           page = null;
@@ -95,7 +111,6 @@ export class ScraperPlaywrightService {
          break;
        }
       }
-      this.logger.debug({ options })
       if (options.autoClose) {
         await page.close();
         page = null;
@@ -110,6 +125,124 @@ export class ScraperPlaywrightService {
       }
       throw error;
     }
+  }
+
+  async evaluateWebsiteWithLazyPage(options:EvaluatePageLazyOptions){
+    let products: Record<string, any>[] = [];
+    //load page;
+    const [, page] = await this.evaluateWebsite({
+      url: options.url,
+      pattern: options.pattern,
+      waitAfterConnected: options.waitAfterConnected,
+      useURL:options.useURL,
+      searchPatternType: options.searchPatternType,
+      searchPattern:options.searchPattern,
+      scrollToView: true,
+      pageNow:options.pageNow,
+    });
+    //
+    let x = 1;
+    do {
+      try {
+        const elLoadMore = await page.$(this.makePattern(options.pattern.containerPattern, options.pattern.patternType));
+        if (elLoadMore) {
+          x++;
+          await elLoadMore.scrollIntoViewIfNeeded();
+          await elLoadMore.click();
+          try {
+            await page.waitForSelector(this.makePattern(options.pattern.containerPattern, options.pattern.patternType));
+          } catch (error) {
+            break;
+          }
+
+        } else {
+          break;
+        }
+      } catch (error) {
+        console.error(error);
+        break;
+      }
+    } while (x < 3);
+    await page.evaluate(() => window.scrollTo(0, 0));
+    // const sh = await page.evaluate(() => document.body.scrollHeight);
+    // do {
+    //   await page.evaluate(() => window.scrollBy(0, Math.floor(Math.random() * 100)));
+    //   await new Promise(res => setTimeout(res, 100));
+    //   const next = await page.$(this.scraper.makePattern(dto.page.containerPattern, dto.page.patternType))
+    //   if(await next.isVisible()){
+    //     break;
+    //   }
+    //   const scrollY = await page.evaluate(()=> window.scrollY);
+    //   if(scrollY>=sh){
+    //     break;
+    //   }
+    // } while (true);
+    await page.waitForSelector(this.makePattern(options.patternProduct.containerPattern, options.patternProduct.patternType), { state: 'attached' });
+    [products] = await this.evaluateWebsite({
+      page,
+      pattern: options.patternProduct,
+      waitAfterConnected: options.waitAfterConnected,
+      autoClose: options.autoClose,
+      scrollToView: true,
+      pageNow:options.pageNow,
+    });
+    return products;
+  }
+
+  async evaluateWebsiteWithLazyReplace(options: EvaluatePageLazyOptions){
+    let products: Record<string, any>[] = [];
+    let page: Page;
+    [products, page] = await this.evaluateWebsite({
+      url: options.url,
+      pattern: options.patternProduct,
+      waitAfterConnected: 0,
+      scrollToView: true,
+      useURL: options.useURL,
+      searchPatternType: options.searchPatternType,
+      searchPattern: options.searchPattern,
+      pageNow: options.pageNow,
+    });
+    let x = 1;
+    do {
+      if (page && !page.isClosed()) {
+        const elLoadMore = await page.$(this.makePattern(options.pattern.containerPattern, options.pattern.patternType));
+        if (elLoadMore) {
+          x++;
+          await elLoadMore.scrollIntoViewIfNeeded();
+          await elLoadMore.click();
+          try {
+            await page.waitForSelector(this.makePattern(options.pattern.containerPattern, options.pattern.patternType));
+          } catch (error) {
+            break;
+          }
+
+        } else {
+          break;
+        }
+        try {
+          await page.waitForLoadState("networkidle");
+          await page.waitForSelector(this.makePattern(options.patternProduct.containerPattern, options.patternProduct.patternType), { state: 'attached' });
+          let nextProducts: Record<string, any>[] = [];
+          [nextProducts, page] = await this.evaluateWebsite({
+            page,
+            pattern: options.patternProduct,
+            waitAfterConnected: 4,
+            scrollToView: true,
+            pageNow: options.pageNow,
+          });
+          products = [...products, ...nextProducts];
+        } catch (error) {
+          console.error(error)
+          break;
+        }
+      } else {
+        break;
+      }
+    } while (x < 3);
+    if (page && !page.isClosed() && options.autoClose) {
+      await page.close();
+    }
+    return products;
   }
 
   makePattern(pattern: string, patternType: "xpath" | "css") {
